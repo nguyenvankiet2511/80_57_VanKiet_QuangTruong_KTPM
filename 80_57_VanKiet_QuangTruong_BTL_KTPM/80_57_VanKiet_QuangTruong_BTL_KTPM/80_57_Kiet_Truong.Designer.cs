e03938fc2126095d284eb0724b62604a4d6442cb
nguyenvankiet2511 <nguyenthikimlyts99@gmail.com>
2024-04-17 10:58:00 +0700

    chuan bi moi truong + TC1 tim kiem am thanh 80_VanKiet

---
+++ 80_57_VanKiet_QuangTruong_BTL_KTPM/80_57_VanKiet_QuangTruong_BTL_KTPM/80_57_Kiet_Truong.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace _80_57_VanKiet_QuangTruong_BTL_KTPM
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnTC1_TKAmThanhTC_80_57_Kiet_Truong = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnTC1_TKAmThanhTC_80_57_Kiet_Truong
+            // 
+            this.btnTC1_TKAmThanhTC_80_57_Kiet_Truong.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.btnTC1_TKAmThanhTC_80_57_Kiet_Truong.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTC1_TKAmThanhTC_80_57_Kiet_Truong.ForeColor = System.Drawing.SystemColors.Info;
+            this.btnTC1_TKAmThanhTC_80_57_Kiet_Truong.Location = new System.Drawing.Point(32, 22);
+            this.btnTC1_TKAmThanhTC_80_57_Kiet_Truong.Name = "btnTC1_TKAmThanhTC_80_57_Kiet_Truong";
+            this.btnTC1_TKAmThanhTC_80_57_Kiet_Truong.Size = new System.Drawing.Size(393, 50);
+            this.btnTC1_TKAmThanhTC_80_57_Kiet_Truong.TabIndex = 0;
+            this.btnTC1_TKAmThanhTC_80_57_Kiet_Truong.Text = "TC1_TimKiemAmThanh_80_57_Kiet_Truong";
+            this.btnTC1_TKAmThanhTC_80_57_Kiet_Truong.UseVisualStyleBackColor = false;
+            this.btnTC1_TKAmThanhTC_80_57_Kiet_Truong.Click += new System.EventHandler(this.btnTC1_TKAmThanhTC_80_57_Kiet_Truong_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1150, 553);
+            this.Controls.Add(this.btnTC1_TKAmThanhTC_80_57_Kiet_Truong);
+            this.Font = new System.Drawing.Font("Times New Roman", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnTC1_TKAmThanhTC_80_57_Kiet_Truong;
+    }
+}
+
---
+++ 80_57_VanKiet_QuangTruong_BTL_KTPM/80_57_VanKiet_QuangTruong_BTL_KTPM/80_57_Kiet_Truong.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using OpenQA.Selenium.Interactions;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace _80_57_VanKiet_QuangTruong_BTL_KTPM
+{
+    public partial class Form1 : Form
+    {
+        private IWebDriver driver_80_57;
+        private const string siteURL = "https://www.shutterstock.com/vi";
+        public Form1()
+        {
+            InitializeComponent();
+            setUp();
+        }
+        private void setUp()
+        {//80_57_VanKiet_QuangTruong
+            var options = new ChromeOptions();
+            options.AddArgument("C:\\Users\\Admin\\AppData\\Local\\Google\\Chrome\\User Data\\Default");
+            options.AddArgument("profile-directory=Default");
+            options.AddArgument("--start-maximized");
+            var service = ChromeDriverService.CreateDefaultService();
+            service.HideCommandPromptWindow = true;
+            driver_80_57 = new ChromeDriver(service, options);
+        }
+        private void navigate()
+        {
+            driver_80_57.Navigate().GoToUrl(siteURL);
+        }
+
+        private void btnTC1_TKAmThanhTC_80_57_Kiet_Truong_Click(object sender, EventArgs e)
+        {
+            //80_57_VanKiet_QuangTruong
+            navigate();//Chạy trang https://www.shutterstock.com/vi
+            driver_80_57.FindElement(By.XPath("/html/body/div[1]/div[3]/div/div/div[1]/div/div/div[2]/div[2]/div[1]/div/div[4]/li")).Click();
+            Thread.Sleep(3000);//đợi trang load
+            driver_80_57.FindElement(By.XPath("/html/body/div[1]/div[3]/div/div/div[1]/div/div/div[2]/div[2]/div[2]/div[3]/div/div/div/button")).Click();
+            Thread.Sleep(3000);//đợi trang load
+            driver_80_57.FindElement(By.CssSelector("input[type='file']")).SendKeys(@"E:\KTPM\video_test\mp3_158kb.mp3");
+        }
+    }
+}
