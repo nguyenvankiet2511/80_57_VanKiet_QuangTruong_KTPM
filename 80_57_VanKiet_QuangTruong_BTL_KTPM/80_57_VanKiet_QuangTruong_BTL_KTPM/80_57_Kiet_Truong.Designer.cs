35f6d112ae3d2504b4ba600f75a2fe5ec87ae7ac
nguyenvankiet2511 <nguyenthikimlyts99@gmail.com>
2024-04-18 14:27:27 +0700

    code tc1+tc2+tc3 bo suu tap+ bao cao 80_VanKiet

--- 80_57_VanKiet_QuangTruong_BTL_KTPM/80_57_VanKiet_QuangTruong_BTL_KTPM/80_57_Kiet_Truong.Designer.cs
+++ 80_57_VanKiet_QuangTruong_BTL_KTPM/80_57_VanKiet_QuangTruong_BTL_KTPM/80_57_Kiet_Truong.Designer.cs
@@ -31,6 +31,9 @@
             this.btnTC1_TKAmThanhTC_80_Kiet = new System.Windows.Forms.Button();
             this.btnTC2_TKAmThanh_SaiDinhDang_80_Kiet = new System.Windows.Forms.Button();
             this.btnTC3_TKAmThanh_SaiDungLuong_80_Kiet = new System.Windows.Forms.Button();
+            this.btn_TC1_LuuVaoBoSuuTapCN_80_VanKiet = new System.Windows.Forms.Button();
+            this.btn_TC2_LuuVaoBoSuuTapMD_80_Kiet = new System.Windows.Forms.Button();
+            this.btn_TC3_BoSuuTap_ChuyenTrangLogin_80_Kiet = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnTC1_TKAmThanhTC_80_Kiet
@@ -72,11 +75,53 @@
             this.btnTC3_TKAmThanh_SaiDungLuong_80_Kiet.UseVisualStyleBackColor = false;
             this.btnTC3_TKAmThanh_SaiDungLuong_80_Kiet.Click += new System.EventHandler(this.btnTC3_TKAmThanh_SaiDungLuong_80_Kiet_Click);
             // 
+            // btn_TC1_LuuVaoBoSuuTapCN_80_VanKiet
+            // 
+            this.btn_TC1_LuuVaoBoSuuTapCN_80_VanKiet.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.btn_TC1_LuuVaoBoSuuTapCN_80_VanKiet.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_TC1_LuuVaoBoSuuTapCN_80_VanKiet.ForeColor = System.Drawing.SystemColors.Info;
+            this.btn_TC1_LuuVaoBoSuuTapCN_80_VanKiet.Location = new System.Drawing.Point(32, 288);
+            this.btn_TC1_LuuVaoBoSuuTapCN_80_VanKiet.Name = "btn_TC1_LuuVaoBoSuuTapCN_80_VanKiet";
+            this.btn_TC1_LuuVaoBoSuuTapCN_80_VanKiet.Size = new System.Drawing.Size(531, 52);
+            this.btn_TC1_LuuVaoBoSuuTapCN_80_VanKiet.TabIndex = 3;
+            this.btn_TC1_LuuVaoBoSuuTapCN_80_VanKiet.Text = "TC1_LuuVaoBoSuuTapCN_80_Kiet";
+            this.btn_TC1_LuuVaoBoSuuTapCN_80_VanKiet.UseVisualStyleBackColor = false;
+            this.btn_TC1_LuuVaoBoSuuTapCN_80_VanKiet.Click += new System.EventHandler(this.btn_TC1_LuuVaoBoSuuTapCN_80_VanKiet_Click);
+            // 
+            // btn_TC2_LuuVaoBoSuuTapMD_80_Kiet
+            // 
+            this.btn_TC2_LuuVaoBoSuuTapMD_80_Kiet.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.btn_TC2_LuuVaoBoSuuTapMD_80_Kiet.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_TC2_LuuVaoBoSuuTapMD_80_Kiet.ForeColor = System.Drawing.SystemColors.Info;
+            this.btn_TC2_LuuVaoBoSuuTapMD_80_Kiet.Location = new System.Drawing.Point(32, 381);
+            this.btn_TC2_LuuVaoBoSuuTapMD_80_Kiet.Name = "btn_TC2_LuuVaoBoSuuTapMD_80_Kiet";
+            this.btn_TC2_LuuVaoBoSuuTapMD_80_Kiet.Size = new System.Drawing.Size(531, 52);
+            this.btn_TC2_LuuVaoBoSuuTapMD_80_Kiet.TabIndex = 4;
+            this.btn_TC2_LuuVaoBoSuuTapMD_80_Kiet.Text = "TC2_LuuVaoBoSuuTapMD_80_Kiet";
+            this.btn_TC2_LuuVaoBoSuuTapMD_80_Kiet.UseVisualStyleBackColor = false;
+            this.btn_TC2_LuuVaoBoSuuTapMD_80_Kiet.Click += new System.EventHandler(this.btn_TC2_LuuVaoBoSuuTapMD_80_Kiet_Click);
+            // 
+            // btn_TC3_BoSuuTap_ChuyenTrangLogin_80_Kiet
+            // 
+            this.btn_TC3_BoSuuTap_ChuyenTrangLogin_80_Kiet.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.btn_TC3_BoSuuTap_ChuyenTrangLogin_80_Kiet.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_TC3_BoSuuTap_ChuyenTrangLogin_80_Kiet.ForeColor = System.Drawing.SystemColors.Info;
+            this.btn_TC3_BoSuuTap_ChuyenTrangLogin_80_Kiet.Location = new System.Drawing.Point(32, 467);
+            this.btn_TC3_BoSuuTap_ChuyenTrangLogin_80_Kiet.Name = "btn_TC3_BoSuuTap_ChuyenTrangLogin_80_Kiet";
+            this.btn_TC3_BoSuuTap_ChuyenTrangLogin_80_Kiet.Size = new System.Drawing.Size(531, 52);
+            this.btn_TC3_BoSuuTap_ChuyenTrangLogin_80_Kiet.TabIndex = 5;
+            this.btn_TC3_BoSuuTap_ChuyenTrangLogin_80_Kiet.Text = "TC3_BoSuuTap_ChuyenTrangLogin_80_Kiet";
+            this.btn_TC3_BoSuuTap_ChuyenTrangLogin_80_Kiet.UseVisualStyleBackColor = false;
+            this.btn_TC3_BoSuuTap_ChuyenTrangLogin_80_Kiet.Click += new System.EventHandler(this.btn_TC3_BoSuuTap_ChuyenTrangLogin_80_Kiet_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1150, 553);
+            this.Controls.Add(this.btn_TC3_BoSuuTap_ChuyenTrangLogin_80_Kiet);
+            this.Controls.Add(this.btn_TC2_LuuVaoBoSuuTapMD_80_Kiet);
+            this.Controls.Add(this.btn_TC1_LuuVaoBoSuuTapCN_80_VanKiet);
             this.Controls.Add(this.btnTC3_TKAmThanh_SaiDungLuong_80_Kiet);
             this.Controls.Add(this.btnTC2_TKAmThanh_SaiDinhDang_80_Kiet);
             this.Controls.Add(this.btnTC1_TKAmThanhTC_80_Kiet);
@@ -92,6 +137,9 @@
         private System.Windows.Forms.Button btnTC1_TKAmThanhTC_80_Kiet;
         private System.Windows.Forms.Button btnTC2_TKAmThanh_SaiDinhDang_80_Kiet;
         private System.Windows.Forms.Button btnTC3_TKAmThanh_SaiDungLuong_80_Kiet;
+        private System.Windows.Forms.Button btn_TC1_LuuVaoBoSuuTapCN_80_VanKiet;
+        private System.Windows.Forms.Button btn_TC2_LuuVaoBoSuuTapMD_80_Kiet;
+        private System.Windows.Forms.Button btn_TC3_BoSuuTap_ChuyenTrangLogin_80_Kiet;
     }
 }
 
