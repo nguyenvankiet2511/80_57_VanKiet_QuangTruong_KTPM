07039a53867aef632f2178a51754174527b7232e
Nguyễn Đình Quang Trường <109427682+truongk3@users.noreply.github.com>
2024-04-21 08:49:52 +0700

    57_QuangTruong

--- 80_57_VanKiet_QuangTruong_BTL_KTPM/80_57_VanKiet_QuangTruong_BTL_KTPM/80_57_Kiet_Truong.cs
+++ 80_57_VanKiet_QuangTruong_BTL_KTPM/80_57_VanKiet_QuangTruong_BTL_KTPM/80_57_Kiet_Truong.cs
@@ -12,12 +12,14 @@ using System.Threading;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 
+
 namespace _80_57_VanKiet_QuangTruong_BTL_KTPM
 {
     public partial class Form1 : Form
     {
         private IWebDriver driver_80_57;
         private const string siteURL = "https://www.shutterstock.com/vi";
+     
         public Form1()
         {
             InitializeComponent();   
@@ -27,18 +29,18 @@ namespace _80_57_VanKiet_QuangTruong_BTL_KTPM
             var options = new ChromeOptions();
             if(profile == 0)
             {
-                options.AddArgument("user-data-dir=C:\\Users\\Admin\\AppData\\Local\\Google\\Chrome\\User Data\\");
-                options.AddArgument("profile-directory=Default");
+                options.AddArgument("user-data-dir=C:\\Users\\T470p\\AppData\\Local\\Google\\Chrome\\User Data\\");
+                options.AddArgument("profile-directory=Profile 8");
             }
             else if(profile == 1)
             {
-                options.AddArgument("user-data-dir=C:\\Users\\Admin\\AppData\\Local\\Google\\Chrome\\User Data\\");
-                options.AddArgument("profile-directory=profile 2");
+                options.AddArgument("user-data-dir=C:\\Users\\T470p\\AppData\\Local\\Google\\Chrome\\User Data\\");
+                options.AddArgument("profile-directory=Profile 2");
             }
             else if (profile == 2)
             {
-                options.AddArgument("user-data-dir=C:\\Users\\Admin\\AppData\\Local\\Google\\Chrome\\User Data\\");
-                options.AddArgument("profile-directory=profile 3");
+                options.AddArgument("user-data-dir=C:\\Users\\T470p\\AppData\\Local\\Google\\Chrome\\User Data\\");
+                options.AddArgument("profile-directory=Profile 3");
             }
             options.AddArgument("--start-maximized");
             var service = ChromeDriverService.CreateDefaultService();
@@ -127,6 +129,8 @@ namespace _80_57_VanKiet_QuangTruong_BTL_KTPM
             driver_80_57.FindElement(By.Id(":rb:")).SendKeys("57");//Tìm và thiết lập giá trị cho ô chiều cao cho phép
             //80_VanKiet
             driver_80_57.FindElement(By.XPath("/html/body/div[5]/div[3]/div/div/div[2]/button")).Click();
+           
+           
         }
 
         private void btn_TC2_TaoThietKe_TenThietKeTrong_80_57_Kiet_Truong_Click(object sender, EventArgs e)
@@ -144,5 +148,143 @@ namespace _80_57_VanKiet_QuangTruong_BTL_KTPM
             //80_VanKiet Tìm kiếm và nhấn Click() vào nút Tạo thiết kế
             driver_80_57.FindElement(By.XPath("/html/body/div[5]/div[3]/div/div/div[2]/button")).Click();
         }
+
+        private void Btn1_TKHinhAnh_Click(object sender, EventArgs e)//upload ảnh thành công
+        {
+            //57_QuangTruong
+           //Chạy trang https://www.shutterstock.com/vi
+            setUp(0);
+            navigate();
+            driver_80_57.FindElement(By.XPath("/html/body/div[1]/div[3]/div/div[2]/div[1]/div/div[1]/div[2]/div[2]/div/div[1]/div/div[1]/li")).Click();
+            Thread.Sleep(3000);// đợi trang load
+
+            driver_80_57.FindElement(By.XPath("/html/body/div[1]/div[3]/div/div[2]/div[1]/div/div[1]/div[2]/div[2]/div/div[2]/div[3]/div/div/div/button")).Click();
+            Thread.Sleep(3000);
+
+            //upload  ảnh 
+            driver_80_57.FindElement(By.CssSelector("input[type='file']")).SendKeys(@"D:\KiemThu\anh1.png");
+        }
+
+        private void Btn2_TKhinhanh_Click(object sender, EventArgs e)// upload ảnh bị sai định dạng
+        {
+            //57_QuangTruong
+
+            setUp(0);
+            navigate();
+            driver_80_57.FindElement(By.XPath("/html/body/div[1]/div[3]/div/div[2]/div[1]/div/div[1]/div[2]/div[2]/div/div[1]/div/div[1]/li")).Click();
+            Thread.Sleep(3000);// đợi trang load
+
+            driver_80_57.FindElement(By.XPath("/html/body/div[1]/div[3]/div/div[2]/div[1]/div/div[1]/div[2]/div[2]/div/div[2]/div[3]/div/div/div/button")).Click();
+            Thread.Sleep(3000);
+
+            //upload  ảnh
+            driver_80_57.FindElement(By.CssSelector("input[type='file']")).SendKeys(@"D:\KiemThu\anh2.webp");
+        }
+
+        private void Btn3_KTHinhAnh_Click(object sender, EventArgs e)// ảnh upload lớn hơn 25MB
+        {
+            //57_QuangTruong
+            setUp(0);
+            navigate();
+            driver_80_57.FindElement(By.XPath("/html/body/div[1]/div[3]/div/div[2]/div[1]/div/div[1]/div[2]/div[2]/div/div[1]/div/div[1]/li")).Click();
+            Thread.Sleep(3000);// đợi trang load
+
+            driver_80_57.FindElement(By.XPath("/html/body/div[1]/div[3]/div/div[2]/div[1]/div/div[1]/div[2]/div[2]/div/div[2]/div[3]/div/div/div/button")).Click();
+            Thread.Sleep(3000);
+            //upload  ảnh
+            driver_80_57.FindElement(By.CssSelector("input[type='file']")).SendKeys(@"D:\KiemThu\anh3.png");
+        }
+
+        private void Btn1_ChuyenDoi_Click(object sender, EventArgs e)// chuyển đổi ảnh thành công
+        {
+            setUp(0);
+            //57_QuangTruong
+            driver_80_57.Navigate().GoToUrl("https://www.shutterstock.com/vi/file-converter"); //Chạy trang 
+           
+            driver_80_57.FindElement(By.CssSelector("input[type='file']")).SendKeys(@"D:\KiemThu\anh1.png"); 
+            //upload  ảnh
+            Thread.Sleep(1000);
+
+            // chọn loại định dạng cần tải xuống
+            driver_80_57.FindElement(By.XPath("/html/body/div[1]/div[2]/div/div/div[2]/div/main/div/div/div/div/div[2]/div/div/div[2]/div/div[2]/div[3]/button")).Click();
+            Thread.Sleep(1000);
+
+            // tải xuống
+            driver_80_57.FindElement(By.XPath("/html/body/div[1]/div[2]/div/div/div[2]/div/main/div/div/div/div/div[2]/div/div/div[2]/div/div[3]/button")).Click();
+            Thread.Sleep(1000);
+        }
+
+        private void Btn2_ChuyenDoi_Click(object sender, EventArgs e)// ảnh chuyển đổi sai định dạng
+        {
+            setUp(0);
+            //57_QuangTruong
+            driver_80_57.Navigate().GoToUrl("https://www.shutterstock.com/vi/file-converter"); //Chạy trang 
+         
+            driver_80_57.FindElement(By.CssSelector("input[type='file']")).SendKeys(@"D:\KiemThu\anh2.webp");    //upload  ảnh
+            Thread.Sleep(1000);
+
+            // chọn loại định dạng cần tải xuống
+            driver_80_57.FindElement(By.XPath("/html/body/div[1]/div[2]/div/div/div[2]/div/main/div/div/div/div/div[2]/div/div/div[2]/div/div[2]/div[1]/button")).Click();
+            Thread.Sleep(1000);
+
+            // tải xuống
+            driver_80_57.FindElement(By.XPath("/html/body/div[1]/div[2]/div/div/div[2]/div/main/div/div/div/div/div[2]/div/div/div[2]/div/div[3]/button")).Click();
+            Thread.Sleep(1000);
+        }
+        private void Btn3_ChuyenDoi_Click(object sender, EventArgs e) // không chọn ảnh cần tải xuống theo định dạng
+        {
+            //57_QuangTruong
+            setUp(0);
+            driver_80_57.Navigate().GoToUrl("https://www.shutterstock.com/vi/file-converter");//Chạy trang 
+            
+            driver_80_57.FindElement(By.CssSelector("input[type='file']")).SendKeys(@"D:\KiemThu\anh4.jpg");
+            Thread.Sleep(1000);
+
+            // tải xuống
+            driver_80_57.FindElement(By.XPath("/html/body/div[1]/div[2]/div/div/div[2]/div/main/div/div/div/div/div[2]/div/div/div[2]/div/div[3]/button")).Click();
+            Thread.Sleep(1000);
+        }
+        private void btn_TC3_TaoThietKe_ThanhCong_80_57_Kiet_Truong_Click(object sender, EventArgs e)// chiều rộng sai
+        {
+            setUp(0);
+            navigate();
+            //57_QuangTruong
+            driver_80_57.FindElement(By.CssSelector("#primaryNavItems > div:nth-child(4) > div")).Click();
+
+            //57_QuangTruong
+            Thread.Sleep(2000);
+            driver_80_57.FindElement(By.CssSelector("#__next > div.MuiContainer-root.MuiContainer-disableGutters.mui-sgxi1e-root > div > div.MuiBox-root.mui-1fjtzvx > div.mui-e11pc7-content > div:nth-child(2) > div.mui-1e99pvw-root > div > div > div:nth-child(1) > div > div > div > div > p")).Click();
+            Thread.Sleep(2000);
+
+            driver_80_57.FindElement(By.CssSelector("input[type='text']")).SendKeys("Quang Trường");//ghi giá trị cho tên ô thiết kế
+            driver_80_57.FindElement(By.XPath("/html/body/div[8]/div[3]/div/div/div[2]/div[2]/div/input")).SendKeys("20");//ghi giá trị cho ô chiều rộng
+            driver_80_57.FindElement(By.XPath("/html/body/div[8]/div[3]/div/div/div[2]/div[3]/div/input")).SendKeys("");// ghi giá trị cho chiều cao
+            Thread.Sleep(3000);
+            //57_QuangTruong
+            driver_80_57.FindElement(By.XPath("/html/body/div[8]/div[3]/div/div/div[2]/button")).Click();
+            
+        }
+
+        private void btn_TC4_TaoThietKe_ThanhCong_80_57_Kiet_Truong_Click(object sender, EventArgs e)// chiều dài sai
+        {
+            setUp(0);
+            navigate();
+            //57_QuangTruong
+            driver_80_57.FindElement(By.CssSelector("#primaryNavItems > div:nth-child(4) > div")).Click();
+
+            //57_QuangTruong
+            Thread.Sleep(2000);
+            driver_80_57.FindElement(By.CssSelector("#__next > div.MuiContainer-root.MuiContainer-disableGutters.mui-sgxi1e-root > div > div.MuiBox-root.mui-1fjtzvx > div.mui-e11pc7-content > div:nth-child(2) > div.mui-1e99pvw-root > div > div > div:nth-child(1) > div > div > div > div > p")).Click();
+            Thread.Sleep(2000);
+
+            driver_80_57.FindElement(By.CssSelector("input[type='text']")).SendKeys("Quang Trường ");//ghi giá trị cho tên ô thiết kế
+            driver_80_57.FindElement(By.XPath("/html/body/div[8]/div[3]/div/div/div[2]/div[2]/div/input")).SendKeys("");//ghi giá trị cho ô chiều rộng
+            driver_80_57.FindElement(By.XPath("/html/body/div[8]/div[3]/div/div/div[2]/div[3]/div/input")).SendKeys("50");// ghi giá trị cho chiều cao
+            Thread.Sleep(3000);
+            //57_QuangTruong
+            driver_80_57.FindElement(By.XPath("/html/body/div[8]/div[3]/div/div/div[2]/button")).Click();
+        }
+
+
     }
 }
