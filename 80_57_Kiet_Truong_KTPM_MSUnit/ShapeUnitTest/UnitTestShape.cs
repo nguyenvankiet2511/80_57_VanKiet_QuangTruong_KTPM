45c565ddbc910b575525615c6375b887afad59a7
nguyenvankiet2511 <nguyenthikimlyts99@gmail.com>
2024-04-22 09:55:35 +0700

    ms unit 80_57_Kiet_Truong

---
+++ 80_57_Kiet_Truong_KTPM_MSUnit/ShapeUnitTest/UnitTestShape.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace ShapeUnitTest
+{
+    public class UnitTestShape
+    {
+    }
+}
---
+++ 80_57_Kiet_Truong_KTPM_MSUnit/UnitTest57_QuangTruong/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTest57_QuangTruong")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTest57_QuangTruong")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("aeb3e563-aed9-462d-beb5-e9c64883b700")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
